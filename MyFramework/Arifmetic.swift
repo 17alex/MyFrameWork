c6fefa0a6cc9fac1d2cabd7b3fda22ee342c0702
Alex <alex171274@rambler.ru>
2021-06-12 21:19:19 +0300

    first

---
+++ MyFramework/Arifmetic.swift
@@ -0,0 +1,17 @@
+//
+//  Arifmetic.swift
+//  MyFramework
+//
+//  Created by Алексей Алексеев on 12.06.2021.
+//
+
+import Foundation
+
+open class Arifmetic {
+    
+    public init() {}
+    
+    open func sum(x: Int, y: Int) -> Int {
+        return x + y
+    }
+}
